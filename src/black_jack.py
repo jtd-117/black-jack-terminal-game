ac30f1b7aec584fa7b35d74376533e5f014db164
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2023-03-06 20:09:28 +1100

    Add suites, ranks & symbols dictionaries

--- src/black_jack.py
+++ src/black_jack.py
@@ -3,3 +3,57 @@
 # @note     GitHub: https://github.com/jtd-117
 # ---------------------------------------------------------------------------- #
 
+# MISCELLANEOUS VARIABLES & LIBRARIES
+
+from random import shuffle
+
+INVALID = "\nINVALID INPUT."
+BORDER = "# ---------------------------------------------------------------------------- #"
+
+BROKE = 0
+ACEVALUE1 = 1
+ACEVALUE2 = 11
+RISKYBOUNDARY = 17
+BLACKJACK = 21
+
+suites = {
+    'Spades': '♠︎',
+    'Clubs': '♣︎',
+    'Hearts': '♥︎',
+    'Diamonds': '♦︎'
+}
+
+ranks = {
+    'Ace': (1,11),
+    'Two': 2, 
+    'Three': 3, 
+    'Four': 4, 
+    'Five': 5, 
+    'Six': 6, 
+    'Seven': 7, 
+    'Eight': 8, 
+    'Nine': 9, 
+    'Ten': 10, 
+    'Jack': 10, 
+    'Queen':10, 
+    'King': 10
+}
+
+symbols = {
+    'Ace': 'A ', 
+    'Two': '2 ', 
+    'Three': '3 ', 
+    'Four': '4 ', 
+    'Five': '5 ', 
+    'Six': '6 ', 
+    'Seven': '7 ', 
+    'Eight': '8 ', 
+    'Nine': '9 ', 
+    'Ten': '10', 
+    'Jack': 'J ', 
+    'Queen': 'Q ', 
+    'King': 'K '
+}
+
+stand_hit = ['s','h']
+yes_no = ['y','n']
