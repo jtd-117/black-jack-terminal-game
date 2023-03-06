86c3c128caa6c888c626c8f8b9f12f2890ddd296
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2023-03-06 20:02:11 +1100

    Add main.py

---
+++ src/main.py
@@ -0,0 +1,13 @@
+# @file	    main.py
+# @author   Jude Thaddeau Data
+# @note     GitHub: https://github.com/jtd-117
+# ---------------------------------------------------------------------------- #
+
+# LIBRARIES:
+import black_jack as bj
+
+# ---------------------------------------------------------------------------- #
+
+# MAIN FUNCTION:
+if __name__ == "__main__":
+        pass
