f5898b60dac01c554b0c2e0ebd13487b9ef33d17
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2023-03-06 20:48:34 +1100

    Add 'Deck' class

--- src/black_jack.py
+++ src/black_jack.py
@@ -60,7 +60,7 @@ yes_no = ['y','n']
 
 # ---------------------------------------------------------------------------- #
 
-class Card():
+class Card(object):
         """
         An INTERFACE for a card from a standard 52-card deck.
         """
@@ -83,3 +83,42 @@ class Card():
                 Prints out the rank & suite of the card.
                 """
                 return self.rank + ' of ' + self.suite
+
+# ---------------------------------------------------------------------------- #
+
+class Deck(object):
+        """
+        An INTERFACE for a standard 52-card deck.
+        """
+
+        def __init__(self):
+                """
+                Initialises a standard 52-card deck.
+                """
+                self.all_cards = []
+                
+        def new_deck(self):
+                """
+                Initialises & shuffles a new deck of cards.
+                """
+
+                # STEP 1: Create new deck
+                print("\nCreating new deck...")
+                for suite in suites.keys():
+                        for rank in ranks.keys():
+                                self.all_cards.append(Card(suite, rank))
+
+                # STEP 2: Shuffle deck
+                print("\nShuffling deck...")
+                shuffle(self.all_cards)
+                return self.all_cards
+
+        def delete_deck(self):
+                """
+                Deletes a deck of cards.
+                """
+                
+                print("\nResetting deck...")
+                self.all_cards = []
+
+# ---------------------------------------------------------------------------- #
